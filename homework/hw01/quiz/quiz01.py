f1dd1c5de99f1d6a99cd5d1e9bd5600390a78754
Leon <zmkoo000@gmail.com>
2017-03-31 12:20:31 +0800

    finished week1 and project1

---
+++ homework/hw01/quiz/quiz01.py
@@ -0,0 +1,34 @@
+def multiple(a, b):
+    """Return the smallest number n that is a multiple of both a and b.
+
+    >>> multiple(3, 4)
+    12
+    >>> multiple(14, 21)
+    42
+    """
+    "*** YOUR CODE HERE ***"
+    i = 1
+    while (i * a) % b:
+        i += 1
+    return i * a
+
+def unique_digits(n):
+    """Return the number of unique digits in positive integer n
+
+    >>> unique_digits(8675309) # All are unique
+    7
+    >>> unique_digits(1313131) # 1 and 3
+    2
+    >>> unique_digits(13173131) # 1, 3, and 7
+    3
+    >>> unique_digits(10000) # 0 and 1
+    2
+    >>> unique_digits(101) # 0 and 1
+    2
+    >>> unique_digits(10) # 0 and 1
+    2
+    """
+    "*** YOUR CODE HERE ***"
+
+    return len(set(str(n)))
+    
---
+++ lab/lab00/lab00.py
@@ -0,0 +1,8 @@
+def twenty_sixteen():
+    """Come up with the most creative expression that evaluates to 2016,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_sixteen()
+    2016
+    """
+    return (1 + 2 + 3 + 4) * 5 * 6 * 7 - 84
